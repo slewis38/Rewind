680be39c575e27b8b8ac199d4586e3391d3a2ea3
Sean Lewis <slewis47@binghamton.edu>
2025-12-08 18:55:26 -0500

    Initial Commit

---
+++ Rewind/RewindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewindApp.swift
+//  Rewind
+//
+//  Created by Sean Lewis on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RewindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RewindTests/RewindTests.swift
@@ -0,0 +1,17 @@
+//
+//  RewindTests.swift
+//  RewindTests
+//
+//  Created by Sean Lewis on 12/8/25.
+//
+
+import Testing
+@testable import Rewind
+
+struct RewindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
